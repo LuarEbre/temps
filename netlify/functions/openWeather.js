80dfb10eb266a9244eee12176e471aa457cf34b5
LuarEbre <raule0822@gmail.com>
2026-03-10 14:48:30 +0100

    Add elevation fetching + Display elevation on UI + Add Separate height unit button

--- netlify/functions/openWeather.js
+++ netlify/functions/openWeather.js
@@ -4,20 +4,39 @@ exports.handler = async function(event) {
     const lon = event.queryStringParameters.lon;
     const API_KEY = process.env.OPEN_WEATHER_KEY;
 
-    const url = `https://pro.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const weatherUrl = `https://pro.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    
+    const elevationUrl = `https://api.open-meteo.com/v1/elevation?latitude=${lat}&longitude=${lon}`;
 
     try {
-        const response = await fetch(url);
+        const [weatherResponse, elevationResponse] = await Promise.all([
+            fetch(weatherUrl),
+            fetch(elevationUrl)
+        ]);
         
-        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+        if (!weatherResponse.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+        const weatherData = await weatherResponse.json();
+        const elevationData = await elevationResponse.json();
+
+        const temperature = weatherData.main ? weatherData.main.temp : null;
+        const timezone = weatherData.timezone ? weatherData.timezone : null;
         
-        const data = await response.json();
+        const elevation = elevationData.elevation ? elevationData.elevation[0] : null;
+
+        const cleanData = {
+            temperature: temperature,
+            timezone: timezone,
+            elevation: elevation
+        };
 
         return {
             statusCode: 200,
-            body: JSON.stringify(data)
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(cleanData)
         };
+
     } catch (error) {
-        return { statusCode: 500, body: JSON.stringify({ error: "Failed fetching Weather" }) };
+        return { statusCode: 500, body: JSON.stringify({ error: "Failed fetching data" }) };
     }
 };
